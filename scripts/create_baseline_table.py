7df58b1787ae30bd110a51de12d51dbf57652496
takuseno <takuma.seno@gmail.com>
2021-08-09 16:17:30 +0900

    Add baseline table

---
+++ scripts/create_baseline_table.py
@@ -0,0 +1,65 @@
+import glob
+import os
+import csv
+
+EXPERT_SCORES = {
+    "halfcheetah": 12135.0,
+    "hopper": 3234.3,
+    "walker2d": 4592.3,
+}
+
+# calculated from d4rl paper
+BASE_RANDOM = {
+    "halfcheetah": -280.178953,
+    "hopper": -20.272305,
+    "walker2d": 1.629008,
+}
+
+TASKS = ["halfcheetah", "hopper", "walker2d"]
+
+DATASET_TYPES = ["random", "medium", "medium_replay", "medium_expert"]
+
+ALGOS = ["cql", "bear"]
+
+
+def compute_normalized_score(raw_score, env):
+    expert_score = EXPERT_SCORES[env]
+    random_score = BASE_RANDOM[env]
+    return (raw_score - random_score) / (expert_score - random_score)
+
+
+def main():
+    table = {}
+    for algo in ALGOS:
+        table[algo] = {}
+        for task in TASKS:
+            table[algo][task] = {}
+            for dataset_type in DATASET_TYPES:
+                base_path = os.path.join("baselines", algo, task, dataset_type)
+                table[algo][task][dataset_type] = []
+                for log_path in glob.glob(f"{base_path}/**/**/**/progress.csv"):
+                    with open(log_path, "r") as f:
+                        reader = csv.reader(f)
+                        results = [row for row in reader]
+                    header = results[0]
+                    index = header.index("evaluation/Average Returns")
+                    table[algo][task][dataset_type].append(float(results[-1][index]))
+
+    with open("baseline_table.csv", "w") as f:
+        writer = csv.writer(f)
+
+        header = ["algo", "env", "dataset", "return", "normalized return"]
+        writer.writerow(header)
+
+        for algo in table.keys():
+            for env in table[algo].keys():
+                for dataset in table[algo][env]:
+                    returns = table[algo][env][dataset]
+                    avg = sum(returns) / len(returns)
+                    row = [algo, env, dataset, avg, 100.0 * compute_normalized_score(avg, env)]
+                    print(row)
+                    writer.writerow(row)
+
+
+if __name__ == '__main__':
+    main()
